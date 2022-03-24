0122d88bb1a3a086e7616a8a6b994a560a69cdff
keenanjon <jon.menna@metropolia.fi>
2022-03-24 10:10:16 +0200

    Initial Commit

---
+++ MoveSenseApp/MoveSenseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveSenseAppApp.swift
+//  MoveSenseApp
+//
+//  Created by Jon Menna on 24.3.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoveSenseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
