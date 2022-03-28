c18d32fcf83e6ac3392ee07ec5616fcd4a9b998e
Miikka Huotari <miikka.huotari@metropolia.fi>
2022-03-28 14:08:39 +0300

    Changed tabview colors

--- MoveSenseApp/Views/Sensors.swift
+++ MoveSenseApp/Views/Sensors.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct Sensors: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.green
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .foregroundColor(Color.white)
+        }
     }
 }
 
