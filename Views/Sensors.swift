f21956dfecb8fb32c9baeb13f9d968c04c174420
Miikka Huotari <miikka.huotari@metropolia.fi>
2022-04-04 13:56:36 +0300

    List view with toggle buttons

--- Views/Sensors.swift
+++ Views/Sensors.swift
@@ -3,14 +3,39 @@
 import SwiftUI
 
 struct Sensors: View {
+    
+    let sensors = ["Accelerometer","Gyroscope","Magnetometer"]
+    @State private var showGreeting = false
+    
     var body: some View {
-        ZStack {
-            Color.blue
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                .foregroundColor(Color.white)
+        NavigationView {
+            List {
+                Section(header:
+                            Text("Sensor 1")) {
+                    ForEach(0 ..< sensors.count, id: \.self) {
+                        Toggle(self.sensors[$0], isOn: $showGreeting)
+                    }
+                }
+                Section(header:
+                            Text("Sensor 2")) {
+                    ForEach(0 ..< sensors.count, id: \.self) {
+                        Toggle(self.sensors[$0], isOn: $showGreeting)
+                    }
+                }
+                Section(header:
+                            Text("Sensor 3")) {
+                    ForEach(0 ..< sensors.count, id: \.self) {
+                        Toggle(self.sensors[$0], isOn: $showGreeting)
+                    }
+                }
+                
+            } .navigationBarTitle("Sensors")
         }
     }
 }
+
+
+
 
 struct Sensors_Previews: PreviewProvider {
     static var previews: some View {
