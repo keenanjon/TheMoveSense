2d7e7c9d70865665412b20936c8f4598bc8c4572
Miikka Huotari <miikka.huotari@metropolia.fi>
2022-04-13 15:31:35 +0300

    Styled home view

--- Views/Home.swift
+++ Views/Home.swift
@@ -6,93 +6,138 @@ struct Home: View {
     @ObservedObject var moveSensecontroller = MoveSenseiController()
     @State private var isDisplayed = false
     @State private var isScanning = false
-    @State  private var toggles: [Bool] = [Bool]()
+    @State private var toggles: [Bool] = [Bool]()
+    
+    init() {
+        UITableView.appearance().backgroundColor = .clear // For tableView
+        UITableViewCell.appearance().backgroundColor = .clear // For tableViewCell
+    }
     
     var body: some View {
         let connectedDevices = moveSensecontroller.connectedPeripherals.map {$0.name}
         NavigationView {
-            VStack {
-                Text("Movesenses")
-                    .font(.largeTitle)
-                    .frame(maxWidth: .infinity, alignment: .center)
+            ZStack {
+                Image("factory")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(minWidth: 0, maxWidth: .infinity)
                 
-                List(bleManager.peripherals) { peripheral in
-                    HStack {
-                        Text(peripheral.name)
-                            .foregroundColor(connectedDevices.contains(peripheral.name) ? .green : .red)
-                            .onTapGesture {
-                                print("Clicked \(peripheral.name)")
-                                
-                                if (connectedDevices.contains(peripheral.name)) {
-                                    moveSensecontroller.disconnect(peripheral: peripheral)
-                                    isDisplayed = false
-                                } else {
-                                    moveSensecontroller.connect(peripheral: peripheral)
-                                    print(moveSensecontroller.objectWillChange)
-                                    isDisplayed = true
+                VStack {
+                    VStack {
+                        Text("Movesenses")
+                            .padding()
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                            .frame(maxWidth: .infinity)
+                            .background(
+                                .gray
+                                    .opacity(0.8)
+                            )
+                    } .cornerRadius(10)
+                    Spacer()
+                    
+                    List(bleManager.peripherals) { peripheral in
+                        HStack {
+                            Text(peripheral.name)
+                                .foregroundColor(connectedDevices.contains(peripheral.name) ? .green : .red)
+                                .onTapGesture {
+                                    print("Clicked \(peripheral.name)")
                                     
+                                    if (connectedDevices.contains(peripheral.name)) {
+                                        moveSensecontroller.disconnect(peripheral: peripheral)
+                                        isDisplayed = false
+                                    } else {
+                                        moveSensecontroller.connect(peripheral: peripheral)
+                                        isDisplayed = true
+                                    }
                                 }
-                            }
-                        
-                        if (connectedDevices.contains(peripheral.name)) {
-                            NavigationLink(destination: Sensors(peripheral: peripheral)) {
-                                Button(action: {
-                                    
-                                }, label: {
-                                    Text("Fetch data")
-                                })
+                            
+                            if (connectedDevices.contains(peripheral.name)) {
+                                NavigationLink(destination: Sensors(peripheral: peripheral)) {
+                                    Button(action: {
+                                    }, label: {
+                                        Text("Fetch data")
+                                    })
+                                }
                             }
                         }
                     }
-                }.frame(height: 300)
-                
-                Spacer()
-                
-                Text("STATUS")
-                    .font(.headline)
-                
-                if bleManager.isSwitchedOn {
-                    Text("Bluetooth is switched on")
-                        .foregroundColor(.green)
-                }
-                else {
-                    Text("Bluetooth is NOT switched on")
-                        .foregroundColor(.red)
-                }
-                
-                Spacer()
-                
-                ZStack {
-                    VStack (spacing: 10) {
-                        if !isScanning {
-                            Button(action: {
-                                for peripheral in bleManager.peripherals where connectedDevices.contains(peripheral.name){
-                                    moveSensecontroller.disconnect(peripheral: peripheral)
-                                }
-                                isScanning = true
-                                self.bleManager.startScanning()
-                            }) {
-                                Text("Start Scanning")
-                            }
-                        }
-                    }.padding()
                     
-                    VStack (spacing: 10) {
-                        if isScanning {
-                            Button(action: {
-                                self.bleManager.stopScanning()
-                                isScanning = false
-                            }) {
-                                Text("Stop Scanning")
+                    Spacer()
+                    
+                    VStack {
+                        if bleManager.isSwitchedOn {
+                            HStack {
+                                Text("Bluetooth:")
+                                Text("ON")
+                                    .foregroundColor(.green)
                             }
                         }
-                    }.padding()
+                        else {
+                            HStack {
+                                Text("Bluetooth:")
+                                Text("OFF")
+                                    .foregroundColor(.red)
+                            }
+                        }
+                    }.font(.headline)
+                    
+                    //                    .padding()
+                    //                    .frame(maxWidth: .infinity)
+                    //                    .background(
+                    //                        .gray
+                    //                        .opacity(0.8)
+                    //                    )
+                    //                    .cornerRadius(10)
+                    
+                    ZStack {
+                        VStack {
+                            if !isScanning {
+                                Button(action: {
+                                    for peripheral in bleManager.peripherals where connectedDevices.contains(peripheral.name){
+                                        moveSensecontroller.disconnect(peripheral: peripheral)
+                                    }
+                                    isScanning = true
+                                    self.bleManager.startScanning()
+                                }) {
+                                    Text("Start Scanning")
+                                        .padding()
+                                        .frame(maxWidth: .infinity)
+                                        .background(
+                                            .gray
+                                                .opacity(0.8)
+                                        )
+                                        .foregroundColor(Color.white)
+                                        .cornerRadius(10)
+                                    
+                                }
+                            }
+                        } .padding(.bottom, 30)
+                        
+                        VStack {
+                            if isScanning {
+                                Button(action: {
+                                    self.bleManager.stopScanning()
+                                    isScanning = false
+                                }) {
+                                    Text("Stop Scanning")
+                                        .padding()
+                                        .frame(maxWidth: .infinity)
+                                        .background(
+                                            .gray
+                                                .opacity(0.8)
+                                        )
+                                        .foregroundColor(Color.white)
+                                        .cornerRadius(10)
+                                }
+                            }
+                        }
+                        .padding(.bottom, 30)
+                    }
                 }
-                Spacer()
-            }
+                .padding()
+            } .navigationBarHidden(true)
         }
     }
 }
-
-
 
